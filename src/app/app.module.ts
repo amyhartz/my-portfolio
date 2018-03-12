8f51e8832e4a645d4fd1ab00acdc341e09173de5
Amy Hartzog <amy.hartzog6@gmail.com>
2018-03-11 21:38:33 -0400

    Add site

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
+
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { AppComponent } from './app.component';
+
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    AngularFontAwesomeModule,
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
