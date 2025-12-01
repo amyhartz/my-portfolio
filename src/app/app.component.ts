8f9130209cded4f904a6acf48ca3cea4ca96d8d3
Amy <amy@macbook-air-m4.local>
2025-12-01 15:12:48 -0500

    Upgrade to angular 13?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import {Technology} from "./Technology";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   title = 'app';å
